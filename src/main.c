3897314fc10e4b1d97fc2bd8d371370c0059035e
Ju Sung Ho <jshs211@naver.com>
2022-12-01 00:53:53 +0900

    hash ver0.0 test - fileread

--- src/main.c
+++ src/main.c
@@ -1,15 +1,45 @@
 #include "main.h"
 #include "ohtable.h"
 
+int file_read(STUDENT* file_mem,char* filename, __uint64* num);
+
 int main()
 {
     char* filename = "student_data.txt";
-    // char err;
-    // int menu;
 
     OHTBL htbl;
+    STUDENT* file_mem=NULL;
+    __uint64 num;
+
+    file_read(file_mem, filename, &num);
     ohtbl_init(&htbl, 100);
 
 
     return 0;
+}
+
+int file_read(STUDENT* file_mem,char* filename, __uint64* num)
+{
+    FILE* fp;
+
+    if((fp = fopen(filename, "r")) == NULL)
+        return ERROR;
+    
+    fscanf(fp,"%lld%*c",num);
+
+    file_mem = (STUDENT*)malloc(sizeof(STUDENT)*(*num));
+     for(int i=0; i<(*num); i++)
+    {
+        fscanf(fp,"%lld%%*c",&file_mem[i].id);
+        fscanf(fp,"%[^|]%*c", file_mem[i].name);
+    
+        for(int j=0; j<MAX_GRADE_NUM; j++)
+        {
+            fscanf(fp,"%f", &file_mem[i].grade[j]);
+        }
+        fscanf(fp,"%f",&file_mem[i].average);
+    }
+
+    fclose(fp);
+    return NO_ERROR;
 }
